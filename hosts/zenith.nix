9a8a56db2df5688f93f869ce668bd21d04f16ab4
donielmaker <daniel.schmidt0204@gmail.com>
2024-11-05 23:18:03 +0100

    added audio

--- hosts/zenith.nix
+++ hosts/zenith.nix
@@ -33,9 +33,11 @@
 
     xdg.portal.enable = true;
 
-    #services.pipewire = {
-    #    enable = true;
-    #    audio.enable = true;
-    #    pulse.enable = true;
-    #};
+    security.rtkit.enable = true;
+    services.pipewire = {
+       enable = true;
+       alsa.enable = true;
+       alsa.support32Bit = true;
+       pulse.enable = true;
+    };
 }
