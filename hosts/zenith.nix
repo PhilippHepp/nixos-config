fb8b064c4ec3aeda6597999f8d86dce6237e6a5f
donielmaker <daniel.schmidt0204@gmail.com>
2024-11-05 22:28:16 +0100

    formatting

--- hosts/zenith.nix
+++ hosts/zenith.nix
@@ -8,16 +8,21 @@
     programs.hyprland.enable = true;
 
     environment.sessionVariables = {
-	WLR_NO_HARDWARE_CURSORS = "1";
-	NIXOS_OZONE_WL = "1";
+        WLR_NO_HARDWARE_CURSORS = "1";
+        NIXOS_OZONE_WL = "1";
     };
 
     hardware = {
-	opengl.enable = true;
+        opengl.enable = true;
 
-	nvidia.modesetting.enable = true;
+        nvidia.modesetting.enable = true;
     };
 
     xdg.portal.enable = true;
-    xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+
+    services.pipewire = {
+        enable = true;
+        audio.enable = true;
+        pulse.enable = true;
+    };
 }
