9073f39e0ddd2800e3370d3042ddfd3ed5662cff
donielmaker <daniel.schmidt0204@gmail.com>
2024-11-07 23:40:35 +0100

    removed pipewire for pulseaudio

--- hosts/zenith.nix
+++ hosts/zenith.nix
@@ -41,12 +41,4 @@
     };
 
     xdg.portal.enable = true;
-
-    # security.rtkit.enable = true;
-    # services.pipewire = {
-    #    enable = true;
-    #    alsa.enable = true;
-    #    alsa.support32Bit = true;
-    #    pulse.enable = true;
-    # };
 }
