513fa2be27164ee7f056a5aa1009817776c9660c
donielmaker <daniel.schmidt0204@gmail.com>
2024-11-07 18:27:17 +0100

    hardcoded tokyonight theme

--- home-manager/modules/alacritty.nix
+++ home-manager/modules/alacritty.nix
@@ -2,8 +2,34 @@
     programs.alacritty = {
         enable = true;
         settings = {
-            # general_import = ["./themes/tokyo_night_storm.toml"];
-            import = ["./alacritty-themes/tokyo_night_storm.toml"];
+            colors = {
+                primary = {
+                    background = "#24283b";
+                    foreground = "#a9b1d6";
+                };
+    
+                normal = {
+                    black   = "#32344a";
+                    red     = "#f7768e";
+                    green   = "#9ece6a";
+                    yellow  = "#e0af68";
+                    blue    = "#7aa2f7";
+                    magenta = "#ad8ee6";
+                    cyan    = "#449dab";
+                    white   = "#9699a8";
+                };
+
+                bright = {
+                    black   = "#444b6a";
+                    red     = "#ff7a93";
+                    green   = "#b9f27c";
+                    yellow  = "#ff9e64";
+                    blue    = "#7da6ff";
+                    magenta = "#bb9af7";
+                    cyan    = "#0db9d7";
+                    white   = "#acb0d0";
+                };
+            };
 
             window = {
                 opacity = 0.8;
@@ -14,6 +40,7 @@
 
             font.normal.family = "FiraCode Nerd Font";
             font.size = 12;
+
         };
     };
 }
