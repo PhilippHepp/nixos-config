b9a05272600a9c33f49b1a628d3ae351e99b19f9
donielmaker <daniel.schmidt0204@gmail.com>
2024-11-07 23:40:20 +0100

    formatting

--- home-manager/modules/themes.nix
+++ home-manager/modules/themes.nix
@@ -1,16 +1,12 @@
 { pkgs, ... }: {
-    qt = {
-        enable = true;
-    };
-
     gtk = {
         enable = true;
 
         cursorTheme.name = "Bibata-Modern-Ice";
         cursorTheme.package = pkgs.bibata-cursors;
 
-        theme.package = pkgs.tokyonight-gtk-theme;
         theme.name = "Tokyonight-Storm-BL";
+        theme.package = pkgs.tokyonight-gtk-theme;
         
     };
 
