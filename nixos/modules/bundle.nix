a67bed8e27664d8dc461c31258a6d5eecba94b6b
donielmaker <daniel.schmidt0204@gmail.com>
2024-11-09 15:19:54 +0100

    seperated configs

--- nixos/modules/bundle.nix
+++ nixos/modules/bundle.nix
@@ -2,5 +2,7 @@
     imports = [
         ./bootloader.nix
         ./pkgs.nix
+        ./sound.nix
+        ./hyprland.nix
     ];
 }
---
+++ nixos/modules/hyprland.nix
@@ -0,0 +1,37 @@
+{
+    services.xserver.videoDrivers = [ "nvidia" ];
+    services.xserver.xkb.layout = "us";
+
+    services.xserver = {
+        enable = true;
+        displayManager.gdm.enable = true;
+        displayManager.gdm.wayland = true;
+    };
+
+    programs.hyprland.enable = true;
+
+    environment.sessionVariables = {
+        WLR_NO_HARDWARE_CURSORS = "1";
+        NIXOS_OZONE_WL = "1";
+    };
+
+    hardware.opengl.enable = true;
+    hardware.opengl.driSupport32Bit = true;
+
+    hardware.nvidia = {
+            modesetting.enable = true;
+
+            powerManagement.enable = false;
+            powerManagement.finegrained = false;
+
+            open = false;
+
+            nvidiaSettings = true;
+
+            package = config.boot.kernelPackages.nvidiaPackages.stable;
+        };
+    };
+
+
+    xdg.portal.enable = true;
+    xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
