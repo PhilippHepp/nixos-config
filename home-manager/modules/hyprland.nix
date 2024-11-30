6ac5c77700f5daa4762115b73cba866a51c6f89a
donielmaker <daniel.schmidt0204@gmail.com>
2024-11-30 18:09:26 +0100

    switched border to match theme

--- home-manager/modules/hyprland.nix
+++ home-manager/modules/hyprland.nix
@@ -74,8 +74,10 @@
                 gaps_in = 5;
                 gaps_out = "60, 20, 20, 20";
                 border_size = 3;
-                "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
-                "col.inactive_border" = "rgba(595959aa)";
+                # "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+                # "col.inactive_border" = "rgba(595959aa)";
+                "col.active_border" = "rgba(7dcfffee)";
+                "col.inactive_border" = "rgba(2F334Caa)";
 
                 layout = "dwindle";
 
