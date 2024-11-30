62102b9b55827e5bf0acc860e1200d4aec49efd5
donielmaker <daniel.schmidt0204@gmail.com>
2024-11-30 18:10:45 +0100

    attempt of fixing tokyonight sddm theem

--- nixos/modules/tokyonight-sddm.nix
+++ nixos/modules/tokyonight-sddm.nix
@@ -1,17 +1,38 @@
-{ pkgs }:
+{
+    qtbase,
+    qtsvg,
+    qtgraphicaleffects,
+    qtquickcontrols2,
+    wrapQtAppsHook,
+    stdenvNoCC,
+    fetchFromGitHub
+}:
 
-pkgs.stdenv.mkDerivation {
+stdenvNoCC.mkDerivation {
     name = "tokyonight-sddm-theme";
 
-    src = pkgs.fetchFromGitHub {
+    src = fetchFromGitHub {
       owner = "siddrs";
       repo = "tokyo-night-sddm";
       rev = "320c8e74ade1e94f640708eee0b9a75a395697c6";
       hash = "sha256-JRVVzyefqR2L3UrEK2iWyhUKfPMUNUnfRZmwdz05wL0=";
     };
 
+    nativeBuildInputs = [
+        wrapQtAppsHook
+    ];
+
+    propagatedUserEnvPkgs = [
+        qtbase
+        qtsvg
+        qtgraphicaleffects
+        qtquickcontrols2
+    ];
+
+    dontBuild = true;
+
     installPhase = ''
-        mkdir -p $out
-        cp -R ./* $out/
+        mkdir -p $out/
+        cp -aR ./* $out
     '';
 }
