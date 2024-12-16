e841bc112138ef769b23e7e17e8bcae055531c6d
donielmaker <daniel.schmidt0204@gmail.com>
2024-12-16 17:58:56 +0100

    fixed unstable packages making problems

--- home-manager/modules/themes.nix
+++ home-manager/modules/themes.nix
@@ -1,21 +1,21 @@
-{ pkgs, ... }: {
-    qt = {
-        enable = true;
-        platformTheme.name = "gtk";
-        style.name = "Catppuccin-Frappe";
-        style.package = pkgs.libsForQt5.qt5ct;
-    };
+{ pkgs-stable, ... }: {
+    # qt = {
+    #     enable = true;
+    #     platformTheme.name = "gtk";
+    #     style.name = "Catppuccin-Frappe";
+    #     style.package = pkgs.libsForQt5.qt5ct;
+    # };
 
     gtk = {
         enable = true;
 
         cursorTheme.name = "Bibata-Modern-Ice";
-        cursorTheme.package = pkgs.bibata-cursors;
+        cursorTheme.package = pkgs-stable.bibata-cursors;
 
         iconTheme.name = "Tokyonight-Dark";
-        iconTheme.package = pkgs.tokyonight-gtk-theme;
+        iconTheme.package = pkgs-stable.tokyonight-gtk-theme;
 
         theme.name = "Tokyonight-Storm-BL-LB";
-        theme.package = pkgs.tokyonight-gtk-theme;
+        theme.package = pkgs-stable.tokyonight-gtk-theme;
     };
 } 
