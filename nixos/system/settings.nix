78b2b104e5a0f94904ff96cefa2d41387c8ec90f
donielmaker <daniel.schmidt0204@gmail.com>
2025-01-12 17:47:30 +0100

    added as global setting

--- nixos/system/settings.nix
+++ nixos/system/settings.nix
@@ -1,6 +1,8 @@
 {config, ...}:
 
 {
+    services.xserver.xkb.layout = config.don.kb_layout;
+
     time.timeZone = config.don.timezone;
 
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
