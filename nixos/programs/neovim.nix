6b63c61ee7eaf67aa409399a48a091bbd62bf73c
donielmaker <daniel.schmidt0204@gmail.com>
2025-01-12 17:46:51 +0100

    moved neovim related pkgs to neovim.nix

--- nixos/programs/neovim.nix
+++ nixos/programs/neovim.nix
@@ -7,8 +7,18 @@
     };
 
     environment.systemPackages = with pkgs-stable; [
-
         pkgs.neovim
+
+        # Programming Stuff
+        zig
+        pkgs.rustc
+        cargo
+        nodejs_22
+        gnumake
+        #openssl
+        pkg-config
+
+        # LSP
         lua-language-server
         rust-analyzer
         tinymist
