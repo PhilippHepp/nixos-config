c37e6e60e53813e30b9eeca3183ca907c647f3b7
donielmaker <daniel.schmidt0204@gmail.com>
2025-01-20 21:04:17 +0100

    moved some variables back into the flake

--- flake.nix
+++ flake.nix
@@ -12,96 +12,17 @@
     outputs = { self, nixpkgs, nixpkgs-stable, home-manager, ... }@inputs :
 
     let 
-        Dlib = import ./lib {inherit inputs;};
+        system = "x86_64-linux";
+
+        pkgs = import inputs.nixpkgs {inherit system; config.allowUnfree = true;};
+        pkgs-stable = import inputs.nixpkgs-stable {inherit system; config.allowUnfree = true;};
+
+        Dlib = import ./lib {inherit inputs system pkgs pkgs-stable;};
     in with Dlib;
 
     {
 
-        #  TODO:this should be defined by the don.username arg
-        # homeConfigurations."donielmaker" = home-manager.lib.homeManagerConfiguration {
-        #     extraSpecialArgs = mainArgs;
-        #     modules = [ 
-        #         {don = ((import ./hosts/galaxia/galaxia.nix) {inherit pkgs-stable;}).don;}
-        #         ./nixos/system/options.nix
-        #         ./home-manager/home.nix 
-        #     ];
-        #     inherit pkgs;
-        # };
-
         nixosConfigurations.galaxia = mkSystem ./hosts/galaxia/galaxia.nix;
-
-        # nixosConfigurations."galaxia" = nixpkgs.lib.nixosSystem {
-        #     specialArgs = mainArgs;
-        #     modules = [ 
-        #         # ./nixos/programs/wireguard.nix
-        #         # System Config
-        #         ./hosts/galaxia/galaxia.nix
-        #
-        #         # Modules
-        #         ./nixos/system/settings.nix
-        #         ./nixos/system/networking.nix
-        #         ./nixos/system/bootloader.nix
-        #         ./nixos/system/user.nix
-        #         ./nixos/system/options.nix
-        #
-        #         ./nixos/programs/pkgs.nix
-        #         ./nixos/programs/neovim.nix
-        #         ./nixos/programs/zsh.nix
-        #         # ./nixos/programs/wireguard.nix
-        #         # ./nixos/netbird.nix
-        #
-        #         ./nixos/hardware/sound.nix
-        #         ./nixos/hardware/hyprland.nix
-        #         ./nixos/hardware/bluethooth.nix
-        #
-        #         ./nixos/displayManagers/sddm.nix
-        #
-        #         ./nixos/gpu/intel.nix
-        #
-        #         ./nixos/hardware/ntfs.nix
-        #
-        #         home-manager.nixosModules.home-manager {
-        #             home-manager.useGlobalPkgs = true;
-        #             home-manager.useUserPackages = true;
-        #             home-manager.extraSpecialArgs = mainArgs;
-        #             home-manager.users.donielmaker = {
-        #                 imports = [ 
-        #                     ./home-manager/home.nix
-        #                     {don = ((import ./hosts/galaxia/galaxia.nix) {inherit pkgs-stable;}).don;}
-        #                     ./nixos/system/options.nix
-        #                 ];
-        #             };
-        #         }
-        #     ];
-        # };
-        #
-        # nixosConfigurations."zenith" = nixpkgs.lib.nixosSystem {
-        #     specialArgs = mainArgs;
-        #     modules = [ 
-        #         # System Config
-        #         ./hosts/zenith/zenith.nix
-        #
-        #         # Modules
-        #         ./nixos/system/settings.nix
-        #         ./nixos/system/networking.nix
-        #         ./nixos/system/bootloader.nix
-        #         ./nixos/system/user.nix
-        #         ./nixos/system/options.nix
-        #
-        #         ./nixos/programs/pkgs.nix
-        #         ./nixos/programs/neovim.nix
-        #         ./nixos/programs/zsh.nix
-        #         # ./nixos/netbird.nix
-        #
-        #         ./nixos/hardware/sound.nix
-        #         ./nixos/hardware/hyprland.nix
-        #         # ./nixos/hardware/bluethooth.nix
-        #
-        #         ./nixos/displayManagers/sddm.nix
-        #
-        #         ./nixos/gpu/nvidia.nix
-        #     ];
-        # };
 
         # devShells.${system}.rust = (import ./testing/rust.nix {inherit pkgs pkgs-stable;});
     };
