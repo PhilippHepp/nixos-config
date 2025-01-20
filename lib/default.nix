18075ba2fe2295b8150fdb93cc4ba842edcb6672
donielmaker <daniel.schmidt0204@gmail.com>
2025-01-20 21:03:56 +0100

    why?

--- lib/default.nix
+++ lib/default.nix
@@ -1,22 +1,17 @@
-{inputs ? null}:
+{inputs ? null, system ? "x86_64-linux", pkgs ? null, pkgs-stable ? null}:
 
 {
     mkSystem = systemConfigPath: 
 
         let
-            system = "x86_64-linux";
-
             systemConfig = import systemConfigPath;
-
-            pkgs = import inputs.nixpkgs {inherit system; config.allowUnfree = true;};
-            pkgs-stable = import inputs.nixpkgs-stable {inherit system; config.allowUnfree = true;};
-
             specialArgs = {inherit pkgs pkgs-stable;} // systemConfig.settings;
         in
 
         inputs.nixpkgs.lib.nixosSystem {
             inherit specialArgs;
-            modules = [(pkgs.lib.filterAttrs (n: v: n == "imports" || n == "system") systemConfig) ] ++ systemConfig.nixosModules ++ [
+            # Disgusting first module please  FIX:
+            modules = [(pkgs.lib.filterAttrs (n: v: n == "imports" || n == "system") systemConfig)] ++ systemConfig.nixosModules ++ [
 
                 inputs.home-manager.nixosModules.home-manager {
                     home-manager = {
