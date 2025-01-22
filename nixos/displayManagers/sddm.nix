52da83ab4a3ab93947a7b3a6755a1fc5cda63777
donielmaker <daniel.schmidt0204@gmail.com>
2025-01-22 17:06:44 +0100

    improved sddm

--- nixos/displayManagers/sddm.nix
+++ nixos/displayManagers/sddm.nix
@@ -1,23 +1,13 @@
 {pkgs, ...}:
 
-let
-    background = pkgs.nixos-artwork.wallpapers.nineish-dark-gray.gnomeFilePath;
-in 
-
 {
-    services.displayManager = {
+    services.displayManager.enable = true;
+    services.displayManager.sddm = {
         enable = true;
-        sddm = {
-            enable = true;
-            wayland.enable = true;
-            theme = "catppuccin-macchiato";
-            package = pkgs.kdePackages.sddm;
-        };
+        wayland.enable = true;
+        theme = "catppuccin-macchiato";
+        package = pkgs.kdePackages.sddm;
     };
 
-    environment.systemPackages = with pkgs; [
-        libsForQt5.qt5.qtgraphicaleffects
-        # (pkgs.elegant-sddm.override {themeConfig.General.background = background;})
-        (pkgs.catppuccin-sddm.override {flavor = "macchiato"; loginBackground = true;})
-    ];
+    environment.systemPackages = [ (pkgs.catppuccin-sddm.override {flavor = "macchiato"; loginBackground = true;}) ];
 }
