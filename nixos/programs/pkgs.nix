1656470a0ed25da1432df0d63119ffd4e9f0738b
donielmaker <daniel.schmidt0204@gmail.com>
2025-02-21 15:49:30 +0100

    added mermaid-cli

--- nixos/programs/pkgs.nix
+++ nixos/programs/pkgs.nix
@@ -7,6 +7,7 @@
     environment.systemPackages = with pkgs-stable; [
         # Testing
         ghostscript
+        mermaid-cli
         mupdf
         (flameshot.override { enableWlrSupport = true; })
         # steam-run
