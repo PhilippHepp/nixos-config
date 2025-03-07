6ba867c5fcd5fee1242b54bda8a785352363bb26
donielmaker <daniel.schmidt0204@gmail.com>
2025-03-07 23:19:28 +0100

    renamed mkSystem to mkNixos

--- lib/mkNixos.nix
+++ lib/mkNixos.nix
@@ -7,12 +7,32 @@ let
     specialArgs = {inherit pkgs pkgs-stable inputs;} // systemConfig.settings;
 in
 
+# inputs.nixpkgs.lib.nixosSystem {
+#     inherit specialArgs;
+#     modules = [
+#         inputs.disko.nixosModules.disko
+#         {system.stateVersion = systemConfig.system.stateVersion;} # Just don't
+#     ] ++ systemConfig.imports ++ systemConfig.nixosModules ++ [
+#
+#         inputs.home-manager.nixosModules.home-manager {
+#             home-manager = {
+#                 useGlobalPkgs = true;
+#                 useUserPackages = true;
+#                 extraSpecialArgs = specialArgs;
+#                 users.donielmaker.imports = systemConfig.hmModules;
+#             };
+#         }
+#     ];
+# }
+
 inputs.nixpkgs.lib.nixosSystem {
     inherit specialArgs;
     modules = [
         inputs.disko.nixosModules.disko
         {system.stateVersion = systemConfig.system.stateVersion;} # Just don't
-    ] ++ systemConfig.imports ++ systemConfig.nixosModules ++ [
+    ] ++ 
+
+    [
 
         inputs.home-manager.nixosModules.home-manager {
             home-manager = {
