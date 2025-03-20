0770307c9812109a2ac8b54a272964ba2f3abe30
donielmaker <daniel.schmidt0204@gmail.com>
2025-03-21 00:03:54 +0100

    improved install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
-echo "Make sure you have the right disk selected!"
-echo "Selected Config: $1"
-echo "Selected Disk: $2"
-echo ""
+printf "Make sure you have the right disk selected!\nFlake:\t%s\nDisk:\t%s\n\n" "$1" "$2"
 
-sudo nix run 'github:nix-community/disko/latest#disko-install' -- --flake '.#$1' --disk main '$2'
+# Wait for User to confirm
+while true; do
+  read -r -p "Press Enter to continue..." key
+  if [[ -z "$key" ]]; then
+    break
+  fi
+done
+
+echo "doing evil"
+
+sudo nix run 'github:nix-community/disko/latest#disko-install' -- --flake ".#$1" --disk main "$2"
