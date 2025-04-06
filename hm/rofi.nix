71d6fcbc588e4931f41a1c94bb9a0de135488ce6
donielmaker <daniel.schmidt0204@gmail.com>
2025-04-06 12:27:04 +0200

    added rofi-catppuccin theme

--- hm/rofi.nix
+++ hm/rofi.nix
@@ -1,10 +1,10 @@
-{pkgs, ...}: 
+{pkgs, inputs, ...}: 
 
 {
     programs.rofi.enable =  true;
     programs.rofi = {
         terminal = "${pkgs.kitty}/bin/kitty";
-        theme = "";
+        # theme = "${inputs.self.packages."x86_64-linux".rofi-catppuccin}/catppuccin-macchiato.rasi";
+        theme = "${inputs.self.packages."x86_64-linux".rofi-catppuccin}/catppuccin-default.rasi";
     };
-
 }
--- testing/rofi-catppuccin.nix
+++ testing/rofi-catppuccin.nix
@@ -1,15 +1,14 @@
 {
     flavor ? "macchiato", 
-    stdenv,
-    fetchFromGitHub,
+    pkgs ? import <nixpkgs> {},
     ...
 }: 
 
-stdenv.mkDerivation {
+pkgs.stdenv.mkDerivation {
     pname = "rofi-catppuccin";
     version = "0.0.1";
 
-    src = fetchFromGitHub {
+    src = pkgs.fetchFromGitHub {
         owner = "catppuccin";
         repo = "rofi";
         rev = "c24a212a6b07c2d45f32d01d7f10b4d88ddc9f45";
@@ -17,9 +16,9 @@ stdenv.mkDerivation {
     };
 
     installPhase = ''
-        #mkdir -p $out
+        mkdir -p $out
 
-        #cp $src/themes/catppuccin-${flavor}.rasi $out/
-        cp $src/themes/catppuccin-${flavor}.rasi $out
+        cp $src/catppuccin-default.rasi $src/themes/catppuccin-${flavor}.rasi $out/
+        sed -i "s/@import \"catppuccin-mocha\"/@import \".\/catppuccin-${flavor}.rasi\"/" $out/catppuccin-default.rasi
     '';
 }
