8f342794273d05090b9fef6f5729218773703ad0
philipp <philipp.hepp7@gmail.com>
2025-04-07 10:55:51 +0200

    added options for switching icons and gtk theme to rose pine

--- hm/themes.nix
+++ hm/themes.nix
@@ -1,7 +1,8 @@
-{ pkgs-stable, ... }: 
+{ pkgs-stable, pkgs, ... }: 
 
 let
     variant = "macchiato";
+  # variant = "rose-pine-moon";
 in
 
 {
@@ -15,13 +16,19 @@ in
     gtk = {
         enable = true;
 
-        cursorTheme.name = "Bibata-Modern-Ice";
-        cursorTheme.package = pkgs-stable.bibata-cursors;
+      # cursorTheme.name = "Bibata-Modern-Ice";
+      # cursorTheme.package = pkgs-stable.bibata-cursors;
+        cursorTheme.name = "BreezeX-RosePine-Linux";
+        cursorTheme.package = pkgs.rose-pine-cursor;
 
         iconTheme.name = "Papirus-Dark";
         iconTheme.package = pkgs-stable.catppuccin-papirus-folders.override {flavor = variant;};
+      # iconTheme.name = "Rose-Pine";
+      # iconTheme.package = pkgs.rose-pine-icon-theme;
 
         theme.name = "catppuccin-macchiato-blue-standard";
         theme.package = pkgs-stable.catppuccin-gtk.override {inherit variant;};
+      # theme.name = "rose-pine-moon";
+      # theme.package = pkgs.rose-pine-gtk-theme.override {inherit variant;};
     };
 } 
