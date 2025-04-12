13463d81bb2d744296c48f40d12299a15e0dc999
philipp <philipp.hepp7@gmail.com>
2025-04-13 00:53:45 +0200

    added nvf bindings

---
+++ hm/vimbinds.nix
@@ -0,0 +1,51 @@
+{
+  config.vim = {
+    comments.comment-nvim.mappings = {
+      toggleCurrentBlock = "gbc";
+      toggleCurrentLine = "gcc";
+    };
+
+    lsp.mappings = {
+      hover = "<S-k>";
+    };
+
+    autocomplete.nvim-cmp.mappings = {
+      confirm = "<C-CR>";
+    };
+
+    tabline.nvimBufferline.mappings = {
+      closeCurrent = "<S-q>";
+      cycleNext = "<S-l>";
+      cyclePrevious = "<S-h>";
+      moveNext = "<C-S-l>";
+      movePrevious = "<C-S-h>";
+    };
+
+    keymaps = [
+      {
+        key = "<leader>h";
+        mode = "n";
+        action = "<CMD>ClangdSwitchSourceHeader<CR>";
+        desc = "Swap between source and header file";
+      }
+      {
+        key = "<leader>n";
+        mode = "n";
+        action = "<CMD>noh<CR>";
+        desc = "Stop highlighting search results";
+      }
+      {
+        key = "<F5>";
+        mode = "n";
+        action = "<CMD>CMakeRun<CR>";
+        desc = "Run CMake project";
+      }
+      {
+        key = "<leader>lq";
+        mode = "n";
+        action = "<CMD>LspRestart<CR>";
+        desc = "Restart LSP";
+      }
+    ];
+  };
+}
